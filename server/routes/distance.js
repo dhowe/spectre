2150626ad977a53f3434d763254fe8b9d921de8f
dhowe <daniel@rednoise.org>
2019-03-28 12:34:40 +0000

    ocean dist

--- server/routes/distance.js
+++ server/routes/distance.js
@@ -1,22 +1,29 @@
-
-function ndistance(oceanA, oceanB) {
-    let total = 0, diff;
-    for (let i = 0; i < a.length; i++) {
-        diff = b[i] - a[i];
-        total += diff * diff;
-    }
-    return Math.sqrt(total);
+function ndistance(a, b) {
+  let total = 0,
+    diff;
+  for (let i = 0; i < a.length; i++) {
+    diff = b[i] - a[i];
+    total += diff * diff;
+  }
+  return Math.sqrt(total);
 }
 
 module.exports = {
   dist: function (req, res) {
-    if (!(req.body.oceanA &&req.body.oceanB)) {
-      res.send('An error occurred: OceanA and OceanB are required parameters');
+    if (!(req.body.oceanA && req.body.oceanB)) {
+      res.send('An error occurred: oceanA and oceanB are required parameters,' +
+        ' {a:' + (typeof oceanA) + ',b:' + (typeof oceanB) + '}');
+      return;
     }
 
-    if (req.body.oceanA.length != 5 || req.body.oceanB.length != 5) {
-      res.send('An error occurred: OceanA and OceanB must be arrays of length 5');
+    let a = req.body.oceanA;
+    let b = req.body.oceanB;
+
+    if (a.length != 5 || b.length != 5) {
+      res.send('An error occurred: oceanA and oceanB must be arrays of length 5', oceanA, oceanB);
+      return;
     }
-    return ndistance(req.body.oceanA, req.body.oceanB);
+
+    res.send(ndistance(a, b));
   }
 };
--- server/tests/routes/distance.test.js
+++ server/tests/routes/distance.test.js
@@ -1,34 +1,42 @@
 const expect = require('chai').expect;
-
 const { dist } = require('../../routes/distance');
 
-let req = {
-    body: {},
-};
+let req = { body: {} };
 
 let res = {
-    sendCalledWith: '',
-    send: function(arg) {
-        this.sendCalledWith = arg;
-    }
+  sendCalledWith: '',
+  send: function (arg) {
+    this.sendCalledWith = arg;
+  }
 };
 
-describe('Distance Route', function() {
-    describe('oceanDist() function', function() {
-        it('Should error out if two args are not provided', function() {
-            dist(req, res);
-            expect(res.sendCalledWith).to.contain('error');
-        });
-        it('Should error out if both are not length 5', function() {
-            dist(req, res);
-            expect(res.sendCalledWith).to.contain('error');
-        });
-        it('Should provided the correct distance in 5d space', function() {
-            let newReq = req;
-            newReq.body.name = 'Jody';
-
-            dist(newReq, res);
-            expect(res.sendCalledWith).to.equal('Hello, Jody');
-        });
-    })
+describe('Distance Route', function () {
+  describe('oceanDist() function', function () {
+    it('Should error if two args are not provided', function () {
+      dist(req, res);
+      expect(res.sendCalledWith).to.contain('error');
+    });
+    it('Should error if both are not length 5 arrays', function () {
+      dist(req, res);
+      expect(res.sendCalledWith).to.contain('error');
+    });
+    it('Should return 0 if arrays are the same', function () {
+      req.body.oceanA = [.5, .5, .5, .5, .5];
+      req.body.oceanB = [.5, .5, .5, .5, .5];
+      dist(req, res);
+      expect(res.sendCalledWith).to.equal(0);
+    });
+    it('Should return dist (fixed)', function () {
+      req.body.oceanA = [.5, .5, .5, .5, 0];
+      req.body.oceanB = [.5, .5, .5, .5, .5];
+      dist(req, res);
+      expect(res.sendCalledWith).to.equal(.5);
+    });
+    it('Should return dist (random)', function () {
+      req.body.oceanA = Array.from({length: 5}, () => Math.random());
+      req.body.oceanB = Array.from({length: 5}, () => Math.random());
+      dist(req, res);
+      expect(res.sendCalledWith).to.be.at.least(0);
+    });
+  })
 });
