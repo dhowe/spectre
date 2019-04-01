ffa83b991bca4417e4d8d7f81d1c634181474a57
dhowe <daniel@rednoise.org>
2019-04-01 22:34:36 +0100

    more passing tests

--- server/predictions.js
+++ server/predictions.js
@@ -1,31 +1,43 @@
-module.exports.oceanSort = function (user, allUsers) {
 
-  var compare = function (a, b) { return a.value - b.value; };
-  var reorder = function (e) { return allUsers[e.index]; };
-  var distances = function (b, i) {
-    return { index: i, value: oceanDist(user, b) };
-  };
-  return allUsers.map(distances).sort(compare).map(reorder);
-}
-
-module.exports.oceanDist = function (a, b) {
+let oceanDist = function (a, b) {
 
   if (!a || !b) throw Error('2 args required');
 
-  if (!a.traits.openness || !b.traits.openness) {
+  if (typeof a.traits.openness === 'undefined' ||
+    typeof b.traits.openness === 'undefined')
+  {
     throw Error('traits required');
   }
 
-  let ta = a.traits, tb = b.traits;
-  let traits = a.traitNames(), diff = 0, total = 0;
+  let ta = a.traits,
+    tb = b.traits;
+  let traits = a.traitNames(),
+    diff = 0,
+    total = 0;
 
   for (let i = 0; i < traits.length; i++) {
     diff = ta[traits[i]] - tb[traits[i]];
-    //console.log(traits[i]+": "+diff);
     total += diff * diff;
   }
 
-  //console.log(Math.sqrt(total));
-
   return Math.sqrt(total);
 }
+
+let oceanSort = function (user, candidates) {
+
+  if (typeof user === 'undefined') throw Error('null user');
+  if (typeof candidates === 'undefined') throw Error('null candidates');
+  if (candidates.length < 1) throw Error('no candidates');
+  candidates = candidates.filter(function(o) {
+    return o._id !== user._id;
+  });
+  let distances = function (b, i) {
+    return { index: i, value: oceanDist(user, b) };
+  };
+  let compare = function (a, b) { return a.value - b.value; };
+  let reorder = function (e) { return candidates[e.index]; };
+  return candidates.map(distances).sort(compare).map(reorder);
+}
+
+module.exports.oceanSort = oceanSort;
+module.exports.oceanDist= oceanDist;
