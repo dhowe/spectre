c0c101076495b8191807ba219722b0f65da69ab6
Joe RN <joseph.allen.1994@hotmail.com>
2019-04-06 17:46:38 +0100

    Add "Touch here" To intro page

--- web-client/src/Components/TouchToBegin/TouchToBegin.jsx
+++ web-client/src/Components/TouchToBegin/TouchToBegin.jsx
@@ -10,16 +10,27 @@ const styles = {
     root: {
         flexGrow: 1,
         width: "100%",
+        position: 'relative',
+        top: 0,
+        left: 0,
     },
     grow: {
         flexGrow: 1,
     },
     TouchToBegin_image: {
+        position: 'relative',
+        top: 0,
+        left: 0,
         minWidth: "100%",
+    },
+    TouchToBegin_logo: {
+        position: 'absolute',
+        top: '40%',
+        left: '50%',
     }
 };
 
-function ButtonAppBar(props) {
+function TouchToBegin(props) {
     const { classes } = props;
     return (
         <div className={classes.root}>
@@ -30,15 +41,17 @@ function ButtonAppBar(props) {
                     </Typography>
                 </Toolbar>
             </AppBar>
-            <Link component={Link} to="/touch-to-begin">
-                <img alt="Touch to Continue" className="TouchToBegin_image" src="https://www.atlantisbahamas.com/media/Things%20To%20Do/Water%20Park/Beaches/Hero/Experiences_Beach.jpg"></img>
+            <Link className={classes.TouchToBegin_images} component={Link} to="/touch-to-begin">
+                <img alt="Touch to Continue" className={classes.TouchToBegin_image} src="https://www.atlantisbahamas.com/media/Things%20To%20Do/Water%20Park/Beaches/Hero/Experiences_Beach.jpg"></img>
+                <img alt="Touch to Continue" className={classes.TouchToBegin_logo} src="https://i.gyazo.com/dedc1236cc349c728e64b134504bf774.png"></img>
             </Link>
+
         </div>
     );
 }
 
-ButtonAppBar.propTypes = {
+TouchToBegin.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(ButtonAppBar);
+export default withStyles(styles)(TouchToBegin);
