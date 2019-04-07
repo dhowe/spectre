9d945ba78beff1e3a5b82dcfa63e21a25b2dcfe6
dhowe <daniel@rednoise.org>
2019-04-07 02:20:39 +0100

    fix auth bug

--- server/config.js
+++ server/config.js
@@ -4,6 +4,6 @@ let env = process.env;
 
 let auth = '';
 let host = env.DB_HOST + ':' + env.DB_PORT + '/' + env.DB_NAME;
-if (env.DB_USER && env.DB_USER.length) env.DB_USER += ':' + env.DB_PASS + '@';
+if (env.DB_USER && env.DB_USER.length) auth = env.DB_USER + ':' + env.DB_PASS + '@';
 
 module.exports = { dburl: 'mongodb://' + auth + host };
