121a19e47ac7c768e755e9cfb547c5a422ab5e9c
joseph-allen <joseph.allen.1994@hotmail.com>
2019-04-15 11:03:25 +0100

    passing test

--- web-client/src/Components/RadioInput/RadioInput.test.jsx
+++ web-client/src/Components/RadioInput/RadioInput.test.jsx
@@ -4,6 +4,6 @@ import RadioInput from './RadioInput';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<RadioInput />, div);
+  ReactDOM.render(<RadioInput options={['Man','Woman','Non-Binary']}>Input Name:</RadioInput>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
