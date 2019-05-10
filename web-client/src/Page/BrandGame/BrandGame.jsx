94dd13a095d603db8cad9640c8c95e01c4895d72
dhowe <daniel@rednoise.org>
2019-05-10 15:34:47 -0400

    removed old BrandGame (now in Game)

--- web-client/src/Page/BrandGame/BrandGame.jsx
+++
@@ -1,2 +0,0 @@
-{/* I've created a canvas prototype of this for my own testing (see /spectre/web-client/src/Canvas/brands/index.html */}
-{/* But it should be probably be ported over to React/DOM, though much of the logic can likely be reused  */}
