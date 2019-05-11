4a262d02e002b9f87e97a057aa6992624279d6fb
dhowe <daniel@rednoise.org>
2019-05-11 19:25:33 -0400

    typo

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
-    "Access-Control-Allow-Headers", "Authorization"
+    "Access-Control-Allow-Headers", "Authorization",
     "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
   );
   if ('OPTIONS' == req.method) {
