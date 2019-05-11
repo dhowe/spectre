47bc05524fb803a516b569a8dc4df0921f5ef5aa
dhowe <daniel@rednoise.org>
2019-05-11 18:21:40 -0400

    more f894cking with CORS

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ const auth = basicAuth({
 });
 
 const allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  // intercept OPTIONS method
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
   if ('OPTIONS' == req.method) {
     res.send(200);
   } else {
