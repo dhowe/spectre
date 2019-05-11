023cffaf3bc78bcfebea4433a74687db8932e3a3
dhowe <daniel@rednoise.org>
2019-05-11 18:25:22 -0400

    still more CORS

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const auth = basicAuth({
 });
 
 const allowCrossDomain = function (req, res, next) {
+  console.log('allowCrossDomain: '+req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
