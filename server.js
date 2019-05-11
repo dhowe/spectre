15d46a4c3f4f1a6a9cb2e54e5d1041f6f2755f6d
dhowe <daniel@rednoise.org>
2019-05-11 19:22:53 -0400

    more corssssss

--- server.js
+++ server.js
@@ -18,14 +18,15 @@ const auth = basicAuth({
 const allowCrossDomain = function (req, res, next) {
   console.log('allowCrossDomain: '+req.headers.origin);
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  //res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
-    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers", "Authorization"
     "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
   );
   if ('OPTIONS' == req.method) {
-    res.send(200);
+    res.sendStatus(200);
   } else {
     next();
   }
