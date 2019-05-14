983673b110e48f60d192b3ac41c1f61881496058
Joe RN <joseph.allen.1994@hotmail.com>
2019-05-14 20:41:19 +0100

    Add Videos for Prototype

--- web-client/src/Page/IntroOceanVideo/IntroOceanVideo.jsx
+++ web-client/src/Page/IntroOceanVideo/IntroOceanVideo.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Video from "../../Components/Video/Video";
 import { Link } from 'react-router-dom';
 import Username from '../Username/Username'
 import IconButton from '../../Components/IconButton/IconButton';
 import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
 import FooterLogo from '../../Components/FooterLogo/FooterLogo';
-
+import movie from "./OCEAN Intro V1.mp4";
 const styles = {
 
 };
@@ -20,13 +19,15 @@ function IntroOceanVideo(props) {
             <SpectreHeader colour="white" />
             <div className="IntroOceanVideo-content content">
                 <Typography>Intro to OCEAN</Typography>
-                <Video></Video>
+                <video width={window.innerWidth} autoplay controls>
+                    <source src={movie} type="video/mp4" />
+                    Your browser does not support the video tag.
+                 </video>
                 <Link component={Username} to="/your-power">
                     <IconButton icon="next" text="Skip" />
                 </Link>
-                
             </div>
-            <FooterLogo />  
+            <FooterLogo />
         </div>
     );
 }
