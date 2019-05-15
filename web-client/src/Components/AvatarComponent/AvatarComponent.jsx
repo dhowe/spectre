039768b0bc7d57ba3454b11a63cb81f77ffe8102
dhowe <dhowe@cims.nyu.edu>
2019-05-15 00:53:28 -0400

    follower-placeholders

--- web-client/src/Components/AvatarComponent/AvatarComponent.jsx
+++ web-client/src/Components/AvatarComponent/AvatarComponent.jsx
@@ -4,43 +4,51 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 class AvatarComponent extends React.Component {
-    styles = {
-        bigAvatar: {
-            margin: 10,
-            width: 60,
-            height: 60,
-        },
-        targeted: {
-            margin: 10,
-            width: 60,
-            height: 60,
-            border: '5px solid #378685',
-        },
-        active: {
-            margin: 10,
-            width: 60,
-            height: 60,
-            border: '5px solid #4FAE50',
-        },
-        targeted_text: {
-            color: '#378685',
-        }
-    };
-
-    constructor(props) {
-        super(props);
-        this.state = { class: this.props.class };
-    };
-
-    render() {
-        return (
-            <div >
-                <Grid container justify="center" alignItems="center">
-                    <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" style={this.state.class === 'active'? this.styles.active : this.styles.bigAvatar} />
-                </Grid>
-                <Typography style={this.state.class === 'targeted' ? this.styles.targeted_text : null}>Remy Sharp</Typography>
-            </div>
-        );
+  styles = {
+    bigAvatar: {
+      margin: 10,
+      width: 60,
+      height: 60,
+    },
+    targeted: {
+      margin: 10,
+      width: 60,
+      height: 60,
+      border: '5px solid #378685',
+    },
+    active: {
+      margin: 10,
+      width: 60,
+      height: 60,
+      border: '5px solid #4FAE50',
+    },
+    targeted_text: {
+      color: '#378685',
     }
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { class: this.props.class }; //, image:  };
+  };
+
+  render() {
+    return (
+      <div >
+          <Grid container justify="center" alignItems="center">
+              <Avatar alt={this.props.target.name} src={this.props.target.image} style={this.state.class === 'active'?    this.styles.active : this.styles.bigAvatar} />
+          </Grid>
+          <Typography style={this.state.class === 'targeted' ? this.styles.targeted_text : null}>{this.props.target.name}</Typography>
+      </div>
+    );
+  }
 }
+
+AvatarComponent.defaultProps = {
+  target: {
+    name: 'Remy Sharp',
+    image: 'https://material-ui.com/static/images/avatar/1.jpg'
+  }
+}
+
 export default AvatarComponent;
--- web-client/src/Page/InfluenceAFollower/InfluenceAFollower.jsx
+++ web-client/src/Page/InfluenceAFollower/InfluenceAFollower.jsx
@@ -11,50 +11,49 @@ import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
 import FooterLogo from '../../Components/FooterLogo/FooterLogo';
 
 const styles = {
-    root: {
-        flexGrow: 1,
-        width: "100%",
+  root: {
+    flexGrow: 1,
+    width: "100%",
 
-        color: 'black'
-    },
-    clickToContinue: {
-        margin: "20% 0",
-    },
-    glow: {
-        color: '#ffd700'
-    }
+    color: 'black'
+  },
+  clickToContinue: {
+    margin: "20% 0",
+  },
+  glow: {
+    color: '#ffd700'
+  }
 };
 
-function InfluenceAFollower(props) {
-    const { classes } = props;
+class InfluenceAFollower extends React.Component {
+  constructor(props) {
+    super(props);
+    this.names = ['Terry', 'Bailey', 'Devin', 'Tyler', 'Fran', 'Pat', 'Sam', 'Reed'];
+  }
+  render() {
     return (
-        <div className={classes.root}>
-            <SpectreHeader colour="white" />
-            <div className={classes.content + " content"}>
-                <Typography component="h3" variant="h3">Influence a follower!</Typography>
-                <Typography component="h4" variant="h4" >Spectre has a community of devout followers willing to help you.</Typography>
-                <Typography component="h5" variant="h5" >Choose one to influence</Typography>
-                <Grid container justify="center" alignItems="center">
-                    <AvatarComponent class='active'></AvatarComponent>
-                    <AvatarComponent></AvatarComponent>
-                    <AvatarComponent></AvatarComponent>
-                    <AvatarComponent></AvatarComponent>
-                    <AvatarComponent></AvatarComponent>
-                    <AvatarComponent></AvatarComponent>
-                    <AvatarComponent></AvatarComponent>
-                    <AvatarComponent></AvatarComponent>
-                </Grid>
-                <Link component={SelectedAvatar} to="/selected-avatar">
-                    <IconButton icon="next" text="Next" />
-                </Link>
-            </div>
-            <FooterLogo />
-        </div>
+      <div className={this.props.classes.root}>
+          <SpectreHeader colour="white" />
+          <div className={this.props.classes.content + " content"}>
+              <Typography component="h3" variant="h3">Influence a follower!</Typography>
+              <Typography component="h4" variant="h4" >Spectre has a community of devout followers willing to help you.</Typography>
+              <Typography component="h5" variant="h5" >Choose one to influence</Typography>
+              <Grid container justify="center" alignItems="center">
+                  {this.names.map((n, i) => <AvatarComponent
+                      target={{name: n, image: '/targets/target'+i+'.png'}}/>)}
+              </Grid>
+              <Link component={SelectedAvatar} to="/selected-avatar">
+                  <IconButton icon="next" text="Next" />
+              </Link>
+          </div>
+          <FooterLogo />
+      </div>
     );
+  }
 }
 
 InfluenceAFollower.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(InfluenceAFollower);
