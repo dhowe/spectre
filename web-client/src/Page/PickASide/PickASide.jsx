b57d7f21f9819083e2f0e7ac27bf5c217015a973
Joe RN <joseph.allen.1994@hotmail.com>
2019-05-16 17:24:17 +0100

    brexit choice

--- web-client/src/Page/PickASide/PickASide.jsx
+++ web-client/src/Page/PickASide/PickASide.jsx
@@ -4,9 +4,9 @@ import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import Campaign from '../Campaign/Campaign';
-import IconButton from '../../Components/IconButton/IconButton';
 import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
 import FooterLogo from '../../Components/FooterLogo/FooterLogo';
+import UserSession from '../../Components/UserSession/UserSession';
 
 const styles = {
     root: {
@@ -21,16 +21,18 @@ const styles = {
 
 function PickASide(props) {
     const { classes } = props;
+    console.log(props)
     return (
         <div className={classes.root}>
             <SpectreHeader colour="white" />
             <div className={classes.content + " content"}>
-
                 <Typography component="h4" variant="h4">What’s your preference?</Typography>
                 <Link component={Campaign} to="/campaign">
-                    <IconButton icon="next" text="Next" />
+                    <img src="https://i.gyazo.com/1890ddef64e0bde795334a2e8564d3b1.png" alt='leave' onClick={() => { this.context.brexitChoice = 'leave' }}></img>
                 </Link>
-
+                <Link component={Campaign} to="/campaign">
+                    <img src="https://i.gyazo.com/de4447b86eac589133ad3ae0e399b5d4.png" alt='remain' onClick={() => { this.context.brexitChoice = 'remain' }}></img>
+                </Link>
             </div >
             <FooterLogo />
         </div >
@@ -40,5 +42,5 @@ function PickASide(props) {
 PickASide.propTypes = {
     classes: PropTypes.object.isRequired,
 };
-
+PickASide.contextType = UserSession;
 export default withStyles(styles)(PickASide);
