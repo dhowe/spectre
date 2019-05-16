9695a7666b426d3b82d0a915a04bc51c5e6c1ed4
aaronalbinson <azza1990uk@yahoo.com>
2019-05-17 00:57:06 +0100

    Set videos to autoplay (#153)

--- web-client/src/Page/IntroVideo/IntroVideo.jsx
+++ web-client/src/Page/IntroVideo/IntroVideo.jsx
@@ -1,38 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '../../Components/IconButton/IconButton';
-import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
-import FooterLogo from '../../Components/FooterLogo/FooterLogo';
-import movie from "./Intro_to_Spectre_V1 .mp4"
-const styles = {
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "../../Components/IconButton/IconButton";
+import SpectreHeader from "../../Components/SpectreHeader/SpectreHeader";
+import FooterLogo from "../../Components/FooterLogo/FooterLogo";
+import movie from "./Intro_to_Spectre_V1 .mp4";
+const styles = {};
 
-};
-
-function IntroVideo(props) {
-    const { classes } = props;
+class IntroVideo extends React.Component {
+  componentDidMount() {
+    this.refs.playVideo.click();
+  }
+  playVideo() {
+    this.refs.video.play();
+  }
+  render() {
+    const { classes } = this.props;
     return (
-        <div className={classes.root}>
-            <SpectreHeader colour="white" />
-            <div className="IntroVideo-content content">
-                {/*<video width={window.innerWidth} autoplay controls>*/}
-                <Typography component="h5" variant="h5">Meet Spectre</Typography>
-                <video width={window.innerWidth} controls>
-                    <source src={movie} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-                <Link to="/username"> <IconButton icon="next" text="Skip" />
-                </Link>
-            </div>
-            <FooterLogo />
+      <div className={classes.root}>
+        <SpectreHeader colour="white" />
+        <div className="IntroVideo-content content">
+          <Typography component="h5" variant="h5">Meet Spectre</Typography>
+          <video ref="video" width={window.innerWidth} controls>
+            <source src={movie} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <button
+            hidden
+            ref="playVideo"
+            id="playButton"
+            onClick={this.playVideo.bind(this)}
+          >
+            Play!
+          </button>
+          <Link to="/username">
+            <IconButton icon="next" text="Skip" />
+          </Link>
         </div>
+        <FooterLogo />
+      </div>
     );
+  }
 }
 
 IntroVideo.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(IntroVideo);
