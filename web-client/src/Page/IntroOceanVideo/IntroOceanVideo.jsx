623ad36578ef2a2b64090d81831614812df8eb34
Aaron <azza1990uk@yahoo.com>
2019-05-17 00:42:46 +0100

    Set videos to autoplay

--- web-client/src/Page/IntroOceanVideo/IntroOceanVideo.jsx
+++ web-client/src/Page/IntroOceanVideo/IntroOceanVideo.jsx
@@ -1,38 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
-import IconButton from '../../Components/IconButton/IconButton';
-import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
-import FooterLogo from '../../Components/FooterLogo/FooterLogo';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import IconButton from "../../Components/IconButton/IconButton";
+import SpectreHeader from "../../Components/SpectreHeader/SpectreHeader";
+import FooterLogo from "../../Components/FooterLogo/FooterLogo";
 import movie from "./OCEAN Intro V1.mp4";
-const styles = {
+const styles = {};
 
-};
-
-function IntroOceanVideo(props) {
-    const { classes } = props;
+class IntroOceanVideo extends React.Component {
+  componentDidMount() {
+    this.refs.playVideo.click();
+  }
+  playVideo() {
+    this.refs.video.play();
+  }
+  render() {
+    const { classes } = this.props;
     return (
-        <div className={classes.root}>
-            <SpectreHeader colour="white" />
-            <div className="IntroOceanVideo-content content">
-                <Typography>Intro to OCEAN</Typography>
-                <video width={window.innerWidth} controls>  {/*autoplay*/}
-                    <source src={movie} type="video/mp4" />
-                    Your browser does not support the video tag.
-                 </video>
-                <Link to="/your-power">
-                    <IconButton icon="next" text="Skip" />
-                </Link>
-            </div>
-            <FooterLogo />
+      <div className={classes.root}>
+        <SpectreHeader colour="white" />
+        <div className="IntroOceanVideo-content content">
+          <Typography>Intro to OCEAN</Typography>
+          <video ref="video" width={window.innerWidth} controls>
+            {" "}
+            {/*autoplay*/}
+            <source src={movie} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <button hidden ref="playVideo" id='playButton' onClick={this.playVideo.bind(this)}>Play!</button>
+          <Link to="/your-power">
+            <IconButton icon="next" text="Skip" />
+          </Link>
         </div>
+        <FooterLogo />
+      </div>
     );
+  }
 }
 
 IntroOceanVideo.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(IntroOceanVideo);
