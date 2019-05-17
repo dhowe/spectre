2d443ee7a0e1908c4c01531dd66d1879cb7fc8c4
Joe RN <joseph.allen.1994@hotmail.com>
2019-05-17 01:58:07 +0100

    influenced by influences

--- web-client/src/Page/InfluencedBy/InfluencedBy.jsx
+++ web-client/src/Page/InfluencedBy/InfluencedBy.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import IconButton from '../../Components/IconButton/IconButton';
 import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
 import FooterLogo from '../../Components/FooterLogo/FooterLogo';
+import UserSession from '../../Components/UserSession/UserSession';
 
 const styles = {
     root: {
@@ -15,27 +16,30 @@ const styles = {
     },
 };
 
-function InfluencedBy(props) {
-    const { classes } = props;
-    return (
-        <div className={classes.root}>
-            <SpectreHeader colour="white" />
-            <div className={classes.content + " content"}>
-                <Typography component="h4" variant="h4">Sophie T ‘s OCEAN profile shows she is most likely influenced by:</Typography>
-                <Typography component="h4" variant="h4">[List of influences]</Typography>
-                <Typography component="h4" variant="h4">Use our design tool to create a targeted Facebook ad to influence {props.selectedFollower.name}’s vote.</Typography>
+class InfluencedBy extends React.Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <SpectreHeader colour="white" />
+                <div className={classes.content + " content"}>
+                    <Typography component="h4" variant="h4">Sophie T ‘s OCEAN profile shows she is most likely influenced by:</Typography>
+                    <Typography component="h4" variant="h4">{this.context.influencedBy}</Typography>
+                    {this.context.influencedBy.map(influence => <p>&#10003;{influence}</p>)}
+                    <Typography component="h4" variant="h4">Use our design tool to create a targeted Facebook ad to influence {this.props.selectedFollower.name}’s vote.</Typography>
 
-                <Link to="/dark-ad">
-                    <IconButton icon="next" text="Let's go" />
-                </Link>
+                    <Link to="/dark-ad">
+                        <IconButton icon="next" text="Let's go" />
+                    </Link>
+                </div>
+                <FooterLogo />
             </div>
-            <FooterLogo />
-        </div>
-    );
+        );
+    }
 }
 
 InfluencedBy.propTypes = {
     classes: PropTypes.object.isRequired,
 };
-
+InfluencedBy.contextType = UserSession;
 export default withStyles(styles)(InfluencedBy);
