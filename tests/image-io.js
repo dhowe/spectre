38a584d10fb66c5c76e59818a5970329591d7811
dhowe <dhowe@cims.nyu.edu>
2019-05-25 15:33:21 -0400

    pre-image-upload

---
+++ tests/image-io.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import chaifs from 'chai-fs';
+import { zipAll } from '../imageio';
+
+const expect = chai.expect;
+chai.use(chaifs);
+
+describe('Image IO', function () {
+
+  describe('ImageIO.zipAll function', function () {
+    it('Should error if no directory is given', function () {
+      expect(() => zipAll(undefined, '')).to.throw();
+    });
+    it('Should zip files in directory', function () {
+      let path = '/tmp/targets.zip';
+      zipAll('web-client/public/targets', path).then(() => {
+        expect(path).to.be.a.path();
+      });
+    });
+  })
+});
