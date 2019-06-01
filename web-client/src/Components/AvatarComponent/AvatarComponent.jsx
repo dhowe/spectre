9a259bdbd5efc79eb4699b2e33b1177a98a3fd42
Paul Jervis <jervis@12os.co.uk>
2019-06-02 00:07:50 +0100

    WIP for styling adjustments, hard work is done, font, colour and pinpoint positionings required.

--- web-client/src/Components/AvatarComponent/AvatarComponent.jsx
+++ web-client/src/Components/AvatarComponent/AvatarComponent.jsx
@@ -2,44 +2,52 @@ import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
+
+const styles = {
+  bigAvatar: {
+    margin: 10,
+    width: 150,
+    height: 150,
+  },
+  targeted: {
+    margin: 10,
+    width: 60,
+    height: 60,
+    border: '5px solid #378685',
+  },
+  active: {
+    margin: 10,
+    width: 60,
+    height: 60,
+    border: '5px solid #4FAE50',
+  },
+  targeted_text: {
+    color: '#378685',
+  },
+};
 
 class AvatarComponent extends React.Component {
-  styles = {
-    bigAvatar: {
-      margin: 10,
-      width: 60,
-      height: 60,
-    },
-    targeted: {
-      margin: 10,
-      width: 60,
-      height: 60,
-      border: '5px solid #378685',
-    },
-    active: {
-      margin: 10,
-      width: 60,
-      height: 60,
-      border: '5px solid #4FAE50',
-    },
-    targeted_text: {
-      color: '#378685',
-    }
-  };
-
   constructor(props) {
     super(props);
-    this.state = { class: this.props.class };
+
+    this.state = { className: props.className };
   }
+
   render() {
+    const { handleClick, target } = this.props;
+    const { className } = this.state;
     return (
-      <div onClick={this.props.handleClick}>
+      <div onClick={handleClick}>
         <Grid container justify="center" alignItems="center">
-          <Avatar alt={this.props.target.name} src={this.props.target.image}
-            style={(this.state.class === 'active' ? this.styles.active : this.styles.bigAvatar)}
-            onError={() => { this.src='/profiles/default.jpg'}} />
+          <Avatar
+            alt={target.name}
+            src={target.image}
+            style={(className === 'active' ? styles.active : styles.bigAvatar)}
+            onError={() => { this.src = '/profiles/default.jpg'; }}
+          />
         </Grid>
-        <Typography style={this.state.class === 'targeted' ? this.styles.targeted_text : null}>{this.props.target.name}</Typography>
+        <Typography style={className === 'targeted' ? styles.targeted_text : null}>{target.name}</Typography>
       </div>
     );
   }
@@ -48,8 +56,14 @@ class AvatarComponent extends React.Component {
 AvatarComponent.defaultProps = {
   target: {
     name: 'Remy Sharp',
-    image: 'https://material-ui.com/static/images/avatar/1.jpg'
-  }
-}
+    image: 'https://material-ui.com/static/images/avatar/1.jpg',
+  },
+  className: null,
+};
+AvatarComponent.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  target: PropTypes.object,
+  className: PropTypes.string,
+};
 
 export default AvatarComponent;
--- web-client/src/Page/InfluenceAFollower/InfluenceAFollower.jsx
+++ web-client/src/Page/InfluenceAFollower/InfluenceAFollower.jsx
@@ -4,72 +4,107 @@ import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { Redirect } from 'react-router-dom';
 import AvatarComponent from '../../Components/AvatarComponent/AvatarComponent';
-import Grid from '@material-ui/core/Grid';
 import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
 import FooterLogo from '../../Components/FooterLogo/FooterLogo';
 import UserSession from '../../Components/UserSession/UserSession';
 
+import './InfluenceAFollower.scss';
+
 const styles = {
   root: {
-    flexGrow: 1,
-    width: "100%",
-    color: 'black'
+    // flexGrow: 1,
+    width: '100%',
+    color: 'black',
   },
   clickToContinue: {
-    margin: "20% 0",
+    margin: '20% 0',
   },
   glow: {
-    color: '#ffd700'
-  }
+    color: '#ffd700',
+  },
 };
 
-const defaults = [{ name: 'Remy', id: '111111111111111111111111' }, { name: 'Bailey', id: '222222222222222222222222' }, { name: 'Devin', id: '333333333333333333333333' }, { name: 'Tyler', id: '444444444444444444444444' }, { name: 'Fran', id: '555555555555555555555555' }, { name: 'Pat', id: '666666666666666666666666' }, { name: 'Sam', id: '777777777777777777777777' }, { name: 'Reed', id: '888888888888888888888888' }, { name: 'Terry', id: '999999999999999999999999' }];
+const defaults = [
+  { name: 'Remy', id: '111111111111111111111111' },
+  { name: 'Bailey', id: '222222222222222222222222' },
+  { name: 'Devin', id: '333333333333333333333333' },
+  { name: 'Tyler', id: '444444444444444444444444' },
+  { name: 'Fran', id: '555555555555555555555555' },
+  { name: 'Pat', id: '666666666666666666666666' },
+  { name: 'Sam', id: '777777777777777777777777' },
+  { name: 'Reed', id: '888888888888888888888888' },
+  { name: 'Terry', id: '999999999999999999999999' },
+];
 
 class InfluenceAFollower extends React.Component {
+  static contextType = UserSession;
+  static generateKey(index) {
+    return (7 * index);
+  }
+
   constructor(props) {
     super(props);
     this.state = { toNext: false };
     this.handleSelect = this.handleSelect.bind(this);
   }
+
   componentWillMount() {
-    let user = this.context;
+    const user = this.context;
+    // eslint-disable-next-line no-underscore-dangle
     if (typeof user === 'undefined' || typeof user._id === 'undefined') { // TMP
       user.name = user.name || 'Barney';
       user.loginType = user.loginType || 'email';
-      user.login = user.login || 'Barney' + (+new Date()) + '@aol.com';
+      user.login = user.login || `Barney${+new Date()}@aol.com`;
       UserSession.createUser(user);
     }
   }
-  renderSimilars() {
-    let result = defaults;
-    let sims = this.context.getSimilars();
-    if (sims && sims.length) result = sims;
-    return result;
-  }
-  renderRedirect() {
-    if (this.state.toNext) {
-      return <Redirect to='/selected-avatar' />
-    }
-  }
+
   handleSelect(target) {
     this.context.targetId = target.id;
     this.context.targetName = target.name;
     this.setState({ toNext: true });
   }
+
+  renderRedirect() {
+    const { toNext } = this.state;
+
+    if (toNext) {
+      return <Redirect to="/selected-avatar" />;
+    }
+    return null;
+  }
+
+  renderSimilars() {
+    let result = defaults;
+    const { context } = this;
+    const sims = context.getSimilars();
+    if (sims && sims.length) {
+      result = sims;
+    }
+    return result.slice(0,6);
+  }
+
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
         {this.renderRedirect()}
-        <SpectreHeader colour="white" progressActive={true} progressNumber="one" />
-        <div className={classes.content + " content"}>
-          <Typography component="h5" variant="h5"><strong>Influence a follower!</strong></Typography>
-          <Typography component="h6" variant="h6" >Spectre has a global community of followers.</Typography>
-          <Typography component="h5" variant="h5" >Choose a participant.</Typography>
-          <Grid container justify="center" alignItems="center">
-            {this.renderSimilars().map((n, i) => <AvatarComponent handleClick={() => this.handleSelect(n)}
-              key={i} target={{ name: n.name, image: '/profiles/' + n.id + '.jpg' }} />)}
-          </Grid>
+        <SpectreHeader colour="white" progressActive progressNumber="one" />
+        <div className={`${classes.content} content`}>
+          <Typography component="h5" variant="h5" className="influence-a-follower"><strong>Influence a follower!</strong></Typography>
+          <Typography component="p" variant="body1" className="community">Spectre has a global community of followers.</Typography>
+          <Typography component="h5" variant="h5" className="choose-participant">Choose a participant:</Typography>
+          <div className="follower-section">
+            <div className="follower-circle">
+              {this.renderSimilars().map((n, i) => (
+                <AvatarComponent
+                  handleClick={() => this.handleSelect(n)}
+                  key={InfluenceAFollower.generateKey(i)}
+                  target={{ name: n.name, image: `/profiles/${n.id}.jpg` }}
+                />
+              ))}
+            </div>
+          </div>
         </div>
         <FooterLogo />
       </div>
@@ -78,8 +113,8 @@ class InfluenceAFollower extends React.Component {
 }
 
 InfluenceAFollower.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.objectOf({}).isRequired,
 };
-InfluenceAFollower.contextType = UserSession;
+//InfluenceAFollower.
 
 export default withStyles(styles)(InfluenceAFollower);
