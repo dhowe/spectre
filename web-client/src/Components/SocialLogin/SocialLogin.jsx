0100077841b1588fa781c0cb9c430f43f9ee6a4b
Stephen Hughes <stephenhughes@macbook15.local>
2019-06-01 18:02:23 +0100

    feature/spectre-180
    Added email validation, still need to get result from database of existing emails
    Also need to verify the form the warning overlay will take

--- web-client/src/Components/SocialLogin/SocialLogin.jsx
+++ web-client/src/Components/SocialLogin/SocialLogin.jsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import UserSession from '../../Components/UserSession/UserSession';
-import Keyboard from 'react-simple-keyboard';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import UserSession from "../../Components/UserSession/UserSession";
+import Keyboard from "react-simple-keyboard";
 
-import grey from '@material-ui/core/colors/grey';
-import 'react-simple-keyboard/build/css/index.css';
-import './SocialLogin.scss';
+import grey from "@material-ui/core/colors/grey";
+import "react-simple-keyboard/build/css/index.css";
+import "./SocialLogin.scss";
 
 const styles = {
+  root: {},
+  context: {},
   textField: {
     width: 300,
     color: grey[50],
-    '&:before': {
-      borderColor: grey[50],
-    },
+    "&:before": {
+      borderColor: grey[50]
+    }
   },
   cssLabel: {
-    transform: 'translate(0,1.5rem)',
+    transform: "translate(0,1.5rem)",
     color: grey[50],
-    '&$cssFocused': {
-      color: grey[50],
-    },
+    "&$cssFocused": {
+      color: grey[50]
+    }
   },
   cssFocused: {
-    '&:after': {
-      borderBottomColor: grey[50],
-    },
+    "&:after": {
+      borderBottomColor: grey[50]
+    }
   },
   cssUnderline: {
-    '&:after': {
-      borderBottomColor: grey[50],
-    },
+    "&:after": {
+      borderBottomColor: grey[50]
+    }
   },
   cssOutlinedInput: {
     // TMP: removed to silence warning in console
@@ -45,49 +47,87 @@ const styles = {
 };
 
 class SocialLogin extends React.Component {
-  handleChange = login => event => {
-    this.context.login = event.target.value; // user-prop
-  }
-  onEmailChange = (input) => {
+  onEmailChange(input) {
+    this.setState({ email: input, emailValid: this.validEmail(input) });
     this.context.login = input;
-    document.getElementsByName('email')[0].value = input;
   }
-  validEmail = (email) => {
-    var re = /^(([^<>()[]\\.,;:\s@"]+(\.[^<>()[]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+
+  handleChange = login => event => {
+    let input = event.target.value;
+    this.setState(
+      {
+        email: input,
+        emailValid: this.validEmail(input)
+      },
+      () => {
+        this.keyboardRef.keyboard.setInput(input);
+      }
+    );
+    this.context.login = event.target.value; // user-prop
+  };
+
+  validEmail = email => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  constructor(props) {
+    super(props);
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.validEmail = this.validEmail.bind(this);
+    this.state = { emailValid: null, email: "" };
   }
+
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.root + " socialLogin"}>
         <div className={classes.content + " socialLogin-content"}>
-          <form onSubmit={this.props.handleSubmit}>
+          <span>
+            {this.state.emailValid || this.state.email.length === 0
+              ? ""
+              : "Please enter a valid email"}
+          </span>
+          <form noValidate onSubmit={this.props.handleSubmit}>
             <FormControl className={classes.margin}>
               <InputLabel
                 classes={{
                   root: classes.cssLabel,
                   focused: classes.cssFocused
-                }}>Email
+                }}
+              >
+                Email
               </InputLabel>
-              <Keyboard onChange={input => this.onEmailChange(input)}/>
+              <Keyboard
+                ref={r => (this.keyboardRef = r)}
+                onChange={input => this.onEmailChange(input)}
+              />
               <Input
-                name='email'
+                name="email"
                 id="custom-css-standard-input"
-                onChange={this.handleChange('name')}
+                value={this.state.email}
+                onChange={this.handleChange("name")}
                 classes={{
                   root: classes.textField,
                   underline: classes.cssUnderline
-                }} /><br />
+                }}
+              />
+              <br />
             </FormControl>
           </form>
-        </div >
+        </div>
       </div>
     );
+  }
+
+  newMethod() {
+    return this;
   }
 }
 
 SocialLogin.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 SocialLogin.contextType = UserSession;
 
