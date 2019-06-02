1e8f0a5e82f7b075a1f8b67999f2a0da371f29fc
dhowe <dhowe@cims.nyu.edu>
2019-06-02 18:08:37 +0100

    fix to regex warning

--- web-client/src/Components/SocialLogin/SocialLogin.jsx
+++ web-client/src/Components/SocialLogin/SocialLogin.jsx
@@ -63,7 +63,7 @@ class SocialLogin extends React.Component {
     this.context.login = event.target.value; // user-prop
   };
   validEmail = email => {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   };
   constructor(props) {
