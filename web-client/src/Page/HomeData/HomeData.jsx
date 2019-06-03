e4cad4809c917f33b8720ca359d4c5e2fe052ffa
aaronalbinson <azza1990uk@yahoo.com>
2019-06-03 03:53:39 +0100

    Data pages icon select (#279)
    * Temporarily enable data page next buttons
    
    * Ability to select 3 or more data items before next enabled

--- web-client/src/Page/HomeData/HomeData.jsx
+++ web-client/src/Page/HomeData/HomeData.jsx
@@ -1,86 +1,108 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import { Link } from 'react-router-dom';
-import IconButton from '../../Components/IconButton/IconButton';
-import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
-import FooterLogo from '../../Components/FooterLogo/FooterLogo';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid/Grid";
+import SpectreHeader from "../../Components/SpectreHeader/SpectreHeader";
+import FooterLogo from "../../Components/FooterLogo/FooterLogo";
+import IconButton from "../../Components/IconButton/IconButton";
+import IconButtonToggle from "../../Components/IconButton/IconButtonToggle";
+
+import "./HomeData.scss";
 
 const styles = {
   root: {
     flexGrow: 1,
     width: "100%",
-    color: 'black'
+    color: "black"
   },
   clickToContinue: {
-    margin: "20% 0",
+    margin: "20% 0"
   },
   button: {
-    borderRadius: '28px',
-    border: 'solid 3px #929391',
-    backgroundColor: '#ffffff',
-    boxShadow: 'none',
-    color: '#929391',
+    borderRadius: "28px",
+    border: "solid 3px #929391",
+    backgroundColor: "#ffffff",
+    boxShadow: "none",
+    color: "#929391"
   },
   link: {
-    display: 'block',
-    marginBottom: '30px',
+    display: "block",
+    marginBottom: "30px"
   },
   row: {
-    flexDirection: 'row',
-    display: 'flex',
-    padding: '0 30% 25% 25%' //tmp
+    flexDirection: "row",
+    display: "flex",
+    padding: "0 30% 25% 25%" //tmp
   },
   column: {
-    flexDirection: 'column',
-    display: 'flex',
+    flexDirection: "column",
+    display: "flex"
+  },
+  item: {
+    flexDirection: "column",
+    display: "flex",
+    padding: "30px"
   },
   icons: {
-    marginBottom: "30px",
+    marginBottom: "30px"
   }
 };
 
-function HomeData(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-            <SpectreHeader
-        colour="white"
-        progressActive={true}
-        progressNumber="two"
-      />
-      <div className={classes.content + " content"}>
-        <Typography component="h6" variant="h6">
-          <strong>Choose 3 types of home data:</strong>
-        </Typography>
-          <div className="HomeDataIcons">
+class HomeData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+
+    this.countAdd = this.countAdd.bind(this);
+  }
+
+  componentDidUpdate() {
+    if (document.getElementsByClassName("iconEnabled").length != this.state.count) {
+      this.setState({ count: document.getElementsByClassName("iconEnabled").length });
+    }
+  }
+
+  countAdd() {
+    this.setState({ count: document.getElementsByClassName("iconEnabled").length });
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <SpectreHeader colour="white" progressActive={true} progressNumber="two" />
+        <div className={classes.content + " content"}>
+          <Typography component="h6" variant="h6">
+            <strong>Choose 3 types of home data:</strong>
+          </Typography>
+
+          <div onClick={this.countAdd} className="HomeDataIcons">
               <Grid className={classes.icons} container>
-                <Grid item sm={4}><IconButton enabled={false} icon="smartwatch" text="Smart Watch" /></Grid>
-                <Grid item sm={4}><IconButton enabled={false} icon="wifi" text="Wifi Router" /></Grid>
-                <Grid item sm={4}><IconButton enabled={false} icon="computers" text="Computers" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="smartwatch" text="Smart Watch" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="wifi" text="Wifi Router" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="computers" text="Computers" /></Grid>
               </Grid>
               <Grid className={classes.icons} container>
-                <Grid item sm={4}><IconButton enabled={false} icon="mobile" text="Mobile Devices" /></Grid>
-                <Grid item sm={4}><IconButton enabled={false} icon="smarttv" text="Smart TV" /></Grid>
-                <Grid item sm={4}><IconButton enabled={false} icon="smartassistant" text="Smart Assistant" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="mobile" text="Mobile Devices" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="smarttv" text="Smart TV" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="smartassistant" text="Smart Assistant" /></Grid>
               </Grid>
           </div>
 
-          <Link to="/find-citizens">
-          {/* <Link to="/find-citizens" className="disabled"></Link> */}
-            <IconButton enabled={false} icon="next" text="Next" />
+          <Link className={this.state.count >= 3 ? "true" : "disabled"} to="/find-citizens">
+            <IconButton enabled={this.state.count >= 3 ? true : false} icon="next" text="Next" />
           </Link>
-
+        </div>
+        <FooterLogo />
       </div>
-      <FooterLogo />
-        </div >
-  );
+    );
+  }
 }
 
 HomeData.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(HomeData);
--- web-client/src/Page/PoliticalData/PoliticalData.jsx
+++ web-client/src/Page/PoliticalData/PoliticalData.jsx
@@ -1,93 +1,113 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import { Link } from 'react-router-dom';
-import IconButton from '../../Components/IconButton/IconButton';
-import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
-import FooterLogo from '../../Components/FooterLogo/FooterLogo';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid/Grid";
+import SpectreHeader from "../../Components/SpectreHeader/SpectreHeader";
+import FooterLogo from "../../Components/FooterLogo/FooterLogo";
+import IconButton from "../../Components/IconButton/IconButton";
+import IconButtonToggle from "../../Components/IconButton/IconButtonToggle";
+
+import "./PoliticalData.scss";
 
 const styles = {
-    root: {
-        flexGrow: 1,
-        width: "100%",
-        color: 'black'
-    },
-    clickToContinue: {
-        margin: "20% 0",
-    },
-    button: {
-        borderRadius: '28px',
-        border: 'solid 3px #929391',
-        backgroundColor: '#ffffff',
-        boxShadow: 'none',
-        color: '#929391',
-    },
-    link: {
-        display: 'block',
-        marginBottom: '30px',
-    },
-    row: {
-        flexDirection: 'row',
-        display: 'flex',
-        padding: '0 30% 25% 25%' //tmp
-    },
-    column: {
-        flexDirection: 'column',
-        display: 'flex',
-    },
-    icons: {
-        marginBottom: "30px",
-    }
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    color: "black"
+  },
+  clickToContinue: {
+    margin: "20% 0"
+  },
+  button: {
+    borderRadius: "28px",
+    border: "solid 3px #929391",
+    backgroundColor: "#ffffff",
+    boxShadow: "none",
+    color: "#929391"
+  },
+  link: {
+    display: "block",
+    marginBottom: "30px"
+  },
+  row: {
+    flexDirection: "row",
+    display: "flex",
+    padding: "0 30% 25% 25%" //tmp
+  },
+  column: {
+    flexDirection: "column",
+    display: "flex"
+  },
+  item: {
+    flexDirection: "column",
+    display: "flex",
+    padding: "30px"
+  },
+  icons: {
+    marginBottom: "30px"
+  }
 };
 
-function PoliticalData(props) {
-    const { classes } = props;
-    return (
-        <div className={classes.root}>
-            <SpectreHeader
-        colour="white"
-        progressActive={true}
-        progressNumber="two"
-      />
-      <div className={classes.content + " content"}>
-        <Typography component="h6" variant="h6">
-          <strong>Choose 3 types of political data:</strong>
-        </Typography>
+class PoliticalData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
 
-          <div className="PoliticalDataIcons">
+    this.countAdd = this.countAdd.bind(this);
+  }
+
+  componentDidUpdate() {
+    if (document.getElementsByClassName("iconEnabled").length != this.state.count) {
+      this.setState({ count: document.getElementsByClassName("iconEnabled").length });
+    }
+  }
+
+  countAdd() {
+    this.setState({ count: document.getElementsByClassName("iconEnabled").length });
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <SpectreHeader colour="white" progressActive={true} progressNumber="two" />
+        <div className={classes.content + " content"}>
+          <Typography component="h6" variant="h6">
+            <strong>Choose 3 types of political data:</strong>
+          </Typography>
+
+          <div onClick={this.countAdd} className="PoliticalDataIcons">
               <Grid className={classes.icons} container>
-                <Grid item sm={4}><IconButton enabled={false} icon="voting" text="Voting Records" /></Grid>
-                <Grid item sm={4}><IconButton enabled={false} icon="finance" text="Financial Records" /></Grid>
-                <Grid item sm={4}><IconButton enabled={false} icon="maps" text="Online Maps" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="voting" text="Voting Records" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="finance" text="Financial Records" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="maps" text="Online Maps" /></Grid>
               </Grid>
               <Grid className={classes.icons} container>
-                <Grid item sm={4}><IconButton enabled={false} icon="political" text="Political Party" /></Grid>
-                <Grid item sm={4}><IconButton enabled={false} icon="polls" text="Polls &amp; Surveys" /></Grid>
-                <Grid item sm={4}><IconButton enabled={false} icon="consultants" text="SConsultants" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="political" text="Political Party" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="polls" text="Polls &amp; Surveys" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="consultants" text="SConsultants" /></Grid>
               </Grid>
               <Grid className={classes.icons} container>
-                <Grid item sm={4}><IconButton enabled={false} icon="bigdata" text="Big Data Sets" /></Grid>
-                <Grid item sm={4}><IconButton enabled={false} icon="internet" text="Internet Platforms" /></Grid>
-                <Grid item sm={4}><IconButton enabled={false} icon="group" text="Group Memberships" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="bigdata" text="Big Data Sets" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="internet" text="Internet Platforms" /></Grid>
+                <Grid item sm={4}><IconButtonToggle enabled={false} icon="group" text="Group Memberships" /></Grid>
               </Grid>
           </div>
 
-          {/* <Link to="/home-data" className="disabled">
-            <IconButton enabled={false} icon="next" text="Next" />
-          </Link> */}
-          <Link to="/home-data">
-            <IconButton enabled={false} icon="next" text="Next" />
+          <Link className={this.state.count >= 3 ? "true" : "disabled"} to="/home-data">
+            <IconButton enabled={this.state.count >= 3 ? true : false} icon="next" text="Next" />
           </Link>
+        </div>
+        <FooterLogo />
       </div>
-      <FooterLogo />
-        </div >
     );
+  }
 }
 
 PoliticalData.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(PoliticalData);
