26b1aaee35c3a8feb5cf63868ea064f5074877ad
dhowe <dhowe@cims.nyu.edu>
2019-06-04 02:55:33 +0100

    quick-fix

--- web-client/src/Page/TargetAd/TargetAd.jsx
+++ web-client/src/Page/TargetAd/TargetAd.jsx
@@ -30,7 +30,7 @@ class TargetAd extends React.Component {
         <Link to="/success-ad">
           <Grid container justify="center">
             <Grid item>
-              <IconButton icon="tick" text="Yes" />
+              <IconButton icon="tick" text="Yes" style={{fontSize:'400px'}}/>
             </Grid>
             <Grid item>
               <IconButton icon="next" text="No" />
