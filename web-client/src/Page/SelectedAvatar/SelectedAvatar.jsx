d6ce1a64ef111e37ab549aebee7c53ac2614b6ff
Aaron <azza1990uk@yahoo.com>
2019-06-04 03:36:30 +0100

    SelectedAvatar page styles

--- web-client/src/Page/SelectedAvatar/SelectedAvatar.jsx
+++ web-client/src/Page/SelectedAvatar/SelectedAvatar.jsx
@@ -2,18 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button/Button';
 import { Link } from 'react-router-dom';
-import IconButton from '../../Components/IconButton/IconButton';
 import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
 import FooterLogo from '../../Components/FooterLogo/FooterLogo';
 import UserSession from '../../Components/UserSession/UserSession';
 import AvatarComponent from '../../Components/AvatarComponent/AvatarComponent';
+import Styles from '../../Styles';
+import colours from '../../colors.scss';
+
+import "./SelectedAvatar.scss";
+
+const height = 53;
+const fontSize = 22;
 
 const styles = {
   root: {
     flexGrow: 1,
     width: "100%",
     color: 'black'
+  },
+  content: {
+    display: 'block',
+  },
+  button: {
+    ...Styles.button,
+    color: colours.blue,
+    borderColor: colours.blue,
+    marginRight: 20,
+    fontSize,
+    height,
+    display: 'inline-block',
+    marginTop: '0'
   },
 };
 
@@ -24,16 +44,18 @@ class SelectedAvatar extends React.Component {
     const tname = target.name || 'Targ';
     const pimg = this.context.targetImgUrl() || '/profiles/default.jpg';
     return (
-      <div className={classes.root}>
+      <div className={classes.root + " SelectedAvatar"}>
         <SpectreHeader colour="white" progressActive={true} progressNumber="one" />
         <div className={classes.content + " content"}>
-          <Typography component="h6" variant="h6">You selected:</Typography>
+          <p className="title">You selected:</p>
+          <div>
           <AvatarComponent target={{ image: pimg }}/>
-          <Typography component="h6" variant="h6"><strong>{tname}</strong></Typography>
-          <Typography component="h6" variant="h6">Lets start by verifying some of the basics to unlock insight into {tname}. </Typography>
-          <Typography component="h6" variant="h6">Don’t worry, only you will see the results. </Typography>
+          <p className="avatarName">{tname}</p>
+          </div>
+          <p className="copy">Lets start by verifying some of the basics to unlock insight into {tname}. </p>
+          <p className="copy">Don’t worry, only you will see the results. </p>
           <Link to="/insight-gender">
-            <IconButton icon="next" text="DIVE IN" />
+            <Button className={classes.button}>Dive in</Button>
           </Link>
         </div>
         <FooterLogo />
