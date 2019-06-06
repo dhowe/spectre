e0d029abac1e5871170d04b673d6c4127f672afc
dhowe <dhowe@cims.nyu.edu>
2019-06-06 10:26:10 +0100

    tweaks

--- shared/text2p.js
+++ shared/text2p.js
@@ -8,7 +8,7 @@ export default {
           'You dislike complexity, and prefer the familiar over the unusual. You\'re quite conservative and choose practical outcomes over creative or imaginative flights.',
           'You are very aware of your feelings -- but not as much your imagination. You both embrace change and resist simultanesouly. Have you forgotten that beauty was once so important to you?',
           'You are intellectually curious and adept at recognizing beauty, no matter the opinions of others. Your imagination is vivid but your creativity can lead you astray',
-          'You\'re more intellectually curious than most, and recognize beauty in the world. But your beliefs are unconventional, and  can tend even toward nihilism.'
+          'You\'re more intellectually curious than most, and recognize beauty in the world. But your beliefs are unconventional, and can tend even toward pessimism.'
         ]
   },
 
@@ -17,10 +17,10 @@ export default {
     poles: ['Impulsive and Spontaneous', 'Organized and Hard-working'],
     meta: 'People scoring high on this trait can be described as organized, reliable, and efficient, while people scoring low on this trait are generally characterized as spontaneous and impulsive.',
     text: [
-          'You are somewhat impulsive, even whimsical... But you are able to make decisions quickly and decisively. Friends might call you colourful, and just generally fun to be with...',
-          'You\'re spontaneous and fun and you like to do unexpected things to keep life interesting. You aren’t unreliable per se, but you have been known to slip up from time to time.',
+          'You are impulsive, even whimsical... But you can act decisively. Friends might call you "fun" to be with, but you\'re no philosopher',
+          'You\'re spontaneous and like to do unexpected things to try to keep life "interesting". You aren’t unreliable per se, but you\'ve been known to slip.',
           'You are random and fun to be around but you can plan when life requires it. Depending on the situation, you can make quick decisions or deliberate for longer if necessary.',
-          'You avoid trouble through planning and imagine that you persistence will get you where you want to go.',
+          'You avoid trouble through planning and imagine that your persistence will get you where you want to go.',
           'You\'re a bit of a perfectionist, planning things to the most minute detail. You can appear somehwat self-satisified, especially when long-term plans come to fruition.'
         ]
   },
@@ -30,11 +30,11 @@ export default {
     poles: ['Contemplative', 'Engaged with Outside World'],
     meta: 'People scoring high on this trait can be described as energetic, talkative and sociable, while people scoring low on this trait tend to be more shy, reserved and comfortable in their own company.',
     text: [
-          'You are quiet and a bit withdrawn, someone who doesn’t need lots of people around to have fun. You even find people tiring at times.',
+          'You are quiet and a bit withdrawn, someone who doesn’t need people around to have \'fun\'. You even find people tiring at times.',
           'You prefer low-key social occasions with a few close friends. It’s not that you are afraid of large parties, they\'re just not that fun for you.',
           'You enjoy and actively seek out social occasions, but don\'t think them super important. You might say that sometimes its nice to take a step back and have a quiet night in.',
-          'You like to think you are energetic and active. You\'re someone who enjoys and actively seeks out social occasions, and especially enjoys talking with groups of people.',
-          'You like to think you are energetic, exuberant and active. You enjoy being the center of attention at social occasions and like to think you\'re ready to step up to any challenge.'
+          'You think you\'re energetic and dynamic. But you\'re someone who seeks out social attention, especially in large groups.',
+          'You think you\'re energetic, exuberant and active. You enjoy being the center of attention at social occasions, but like to think you\'re ready for something bigger.'
         ]
   },
 
@@ -43,10 +43,10 @@ export default {
     poles: ['Competitive', 'Team-working and Trusting'],
     meta: 'People scoring high on this trait are generally considered soft-hearted, generous, and sympathetic, while people scoring low on this trait tend to be more driven, self-confident and competitive.',
     text: [
-          'You are willing to make tough decisions when necessary, and will point out when something\'s wrong, no matter what others  think. You are tough and uncompromising.',
-          'You often find it difficult to get along with new people you meet, as you can be suspicious of their motives. Over time, however, you make good connections, though this doesn’t stop your from telling them \'how it is\'.',
+          'You can make tough decisions when necessary, and are happy to point out when something\'s wrong. You are tough, but somewhat uncompromising.',
+          'You often have a hard time with new people, as you can be suspicious of their motives. Over time, however, you make good connections. But it doesn’t stop your from telling them \'how it is\'.',
           'You get along with people, especially once they\'ve proved themselves trustworthy. You have a healthy scepticism about others’ motives, but that doesn’t stop you from considering them basically honest and decent.',
-          'You\'re someone people get along with easily. You\'re considerate, friendly, and generally expect others to be honest and decent.',
+          'You\'re someone people think they can get along with easily. You generally expect others to be honest and decent, even when they\'re not',
           'You are easy to get along with (most of the time). You imagine yourself friendly and tolerant, but this doesn\'t always extend to those who are not as enlightened'
         ]
   },
@@ -56,8 +56,8 @@ export default {
     poles: ['Laid-back and Relaxed', 'Easily Stressed and Emotional'],
     meta: 'People scoring high on this trait generally worry more than most, and react poorly to stressful situations. However, they often show an emotional depth that others lack.',
     text: [
-          'You rarely show negative emotions even when you feel anxious. You try to present yourself calm and resilient.',
-          'You\'re emotionally stable, try to act as you\'re not bothered by things.',
+          'You don\'t show negative emotions even when you feel anxious. You try to present yourself as calm and resilient.',
+          'You\'re generally emotionally stable, but try too hard to act as if you\'re never bothered.',
           'You are generally calm. Though you can feel emotional or stressed out by some experiences, your feelings tend to be warranted by the situation.',
           'You are more self-conscious than some, and can get caught up in stressful situations.  On the other hand you\'re quite \"in touch\" with your own feelings.',
           'You are likely to react poorly to stressful situations. On the other hand you demonstrate a somewhat unexpected emotional depth.'
