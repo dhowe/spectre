eaddfee50927577062a074585b77c23ac7c7a163
Kenny Wong <kennyviperhk@gmail.com>
2020-01-21 13:17:09 +0800

    removed warnings

--- client/src/Page/ConsumerData/ConsumerData.jsx
+++ client/src/Page/ConsumerData/ConsumerData.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+//import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid/Grid';
 import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
