49faceafa9d9f4641229f69fb82ca5d15d5cdd96
dhowe <dhowe@cims.nyu.edu>
2020-02-26 13:48:55 +0800

    adjustment of styles/text in TOS modal

--- client/src/Pages/TakeBackControl/TakeBackControl.jsx
+++ client/src/Pages/TakeBackControl/TakeBackControl.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-//import Typography from '@material-ui/core/Typography';
-//import Grid from '@material-ui/core/Grid';
-//import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
 import FooterLogo from '../../Components/FooterLogo/FooterLogo';
