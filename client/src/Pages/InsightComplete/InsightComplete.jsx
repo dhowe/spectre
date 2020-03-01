2288f8b826bc03e8ea33780f0238a2f4977b0b45
dhowe <dhowe@cims.nyu.edu>
2020-03-02 01:02:30 +0800

    comment

--- client/src/Pages/InsightComplete/InsightComplete.jsx
+++ client/src/Pages/InsightComplete/InsightComplete.jsx
@@ -6,7 +6,7 @@ import IconButton from '../../Components/IconButton/IconButton';
 import SpectreHeader from '../../Components/SpectreHeader/SpectreHeader';
 import FooterLogo from '../../Components/FooterLogo/FooterLogo';
 import UserSession from '../../Components/UserSession/UserSession';
-import { ReactComponent as ThumbUp } from '../../Icons/insightthankyou.svg';
+//import { ReactComponent as ThumbUp } from '../../Icons/insightthankyou.svg';
 import Video from '../../Components/Video/Video';
 import './InsightComplete.scss';
 import IdleChecker from '../../Components/IdleChecker/IdleChecker';
