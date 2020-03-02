17d2406d9ef019b5f4370746960dbb65c21d508b
Kenny Wong <kennyviperhk@gmail.com>
2020-03-02 16:36:58 +0800

    icon fix

--- client/src/Pages/ThankYou/ThankYou.jsx
+++ client/src/Pages/ThankYou/ThankYou.jsx
@@ -29,10 +29,9 @@ class ThankYou extends React.Component {
         <IdleChecker />
         <div className={`${classes.content} content`}>
           <div>
-            <h1 className="addSpacing">Thank you, <span>{user.name}!</span></h1>
-
-            <p className="normal-addSpacing">Your experience has been <strong>personalised.</strong></p>
             <ThumbUp className="thankyou-icon" />
+            <h1 className="addSpacing">Thank you, <span>{user.name}!</span></h1>
+            <p className="normal-addSpacing">Your experience has been <strong>personalised.</strong></p>
           </div>
 
           <div className="link">
