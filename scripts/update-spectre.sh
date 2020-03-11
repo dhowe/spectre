5c2061993375ab3667deb118f02647f1f6437e81
dhowe <daniel@rednoise.org>
2020-03-11 19:48:22 +0800

    added git stash

--- scripts/update-spectre.sh
+++ scripts/update-spectre.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 cd ~/git/spectre
+/usr/local/bin/git stash
 /usr/local/bin/git pull
-/usr/local/Homebrew/bin/yarn install
+/usr/local/Homebrew/bin/yarn install-all
 /usr/local/Homebrew/bin/yarn pub-client
 /usr/local/Homebrew/bin/pm2 reload ecosystem.config.js --env=production --update-env
